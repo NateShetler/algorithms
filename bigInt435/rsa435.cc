a05e3ff41b683af8879635c0ff6c0525aeac49fd
NateShetler <nateshetler21@gmail.com>
2020-02-03 15:32:25 -0500

    Add partially finished implementation of fermats test

--- bigInt435/rsa435.cc
+++ bigInt435/rsa435.cc
@@ -5,15 +5,15 @@
 #include <iostream>
 #include <stdlib.h> 
 #include <fstream>
+#include <time.h>
 
 
 // 'BigIntegerLibrary.hh' includes all of the library headers.
 #include "BigIntegerLibrary.hh"
 
-// Pre: This function accepts a BigUnsigned number that will be checked to see if it is prime, 
-// and also an integer for the number of iterations to try and see if the number is prime.
+// Pre: This function accepts a BigUnsigned number that will be checked to see if it is prime.
 // Post: This function will return true if the number is prime and false if it is not. 
-bool fermatTest(BigUnsigned &number,  int &numIterations);
+bool fermatTest(BigUnsigned &number);
 
 int main(){
 	/* The library throws 'const char *' error messages when things go
@@ -25,45 +25,49 @@ int main(){
     std::ofstream pqFile;
 
 	try {
+      
+      // Seeding so numbers are more random
+      srand(time(NULL));
 
       // Open the file for storing p & q
       pqFile.open("p_q.txt");
       
-      std::cout << "a couple of test cases for 3460:435/535 Algorithms!!!\n";
-      BigUnsigned big1 = BigUnsigned(1);
-      for (int i=0;i<400;i++) {
-         big1 = big1*10 +rand();
+      // Generateing P
+      BigUnsigned pNumber = BigUnsigned(1);
+      for (int i = 0; i < 146; i++) {
+         pNumber = pNumber * 10 + rand();
       }
-      std::cout << "my big1 !!!\n";
-      std::cout << big1 <<std::endl;
+      std::cout << "This is P\n";
+      std::cout << pNumber <<std::endl;
 
-      // Write big1 to pqFile 
-      pqFile << big1 << "\n";
+      // Write pNumber to pqFile 
+      pqFile << pNumber << "\n";
 
-      BigUnsigned big2 = BigUnsigned(1);
-      for (int i=0;i<400;i++) {
-         big2 = big2*10 +rand();
+      // Generating Q
+      BigUnsigned qNumber = BigUnsigned(1);
+      for (int i = 0; i < 146; i++) {
+         qNumber = qNumber * 10 + rand();
       }
-      std::cout << "my big2 !!!\n";
-      std::cout << big2 << std::endl;
+      std::cout << "This is Q\n";
+      std::cout << qNumber << std::endl;
 
-      // Write big2 to pqFile 
-      pqFile << big2 << "\n";
+      // Write qNumber to pqFile 
+      pqFile << qNumber << "\n";
 
-      std::cout << "my big3 = big1*big2 !!!\n";
+      std::cout << "N = P * Q\n";
 
-      // big3 is n
-      BigUnsigned big3 = big1*big2;
-      std::cout << big3 << std::endl;
+      // Generating N
+      BigUnsigned nNumber = pNumber * qNumber;
+      std::cout << nNumber << std::endl;
 
       // This is phi of n
-      BigUnsigned phiOfn = (big1 - 1) * (big2 - 1);
-
-      /*
-      std::cout << "my big3/big2 !!!\n";
-      std::cout <<big3/big2 << std::endl;
-      */
+      BigUnsigned phiOfn = (pNumber - 1) * (qNumber - 1);
       
+      // This is a test block for checking the fermatTest function. REMOVE AFTER TESTING--------
+      bool isPrime = false;
+      isPrime = fermatTest(pNumber);
+      std::cout << isPrime << std::endl;
+
 	} catch(char const* err) {
 		std::cout << "The library threw an exception:\n"
 			<< err << std::endl;
@@ -72,10 +76,37 @@ int main(){
 	return 0;
 }
 
-// Pre: This function accepts a BigUnsigned number that will be checked to see if it is prime, 
-// and also an integer for the number of iterations to try and see if the number is prime.
+// Pre: This function accepts a BigUnsigned number that will be checked to see if it is prime.
 // Post: This function will return true if the number is prime and false if it is not. 
-bool fermatTest(BigUnsigned &number,  int &numIterations)
+bool fermatTest(BigUnsigned &number)
 {
-   
+   // This is the exponent value (p - 1)
+   BigUnsigned exponent = number - 1;
+
+   // This will be the answer returned by modex (the remainder)
+   BigUnsigned answer = 0;
+
+   answer = modexp(2, exponent, number);
+
+   // REMOVE AFTER TESTING-------------------------------------
+   std::cout << "modexp answer 1: " << answer << std::endl;
+
+   // If the remainder is not 1 return false
+   if (answer != 1)
+   { 
+      return false;
+   }
+
+   // Otherwise we will try again
+   answer = modexp(5, exponent, number);
+
+   // If the remainder is not 1 return false
+   if (answer != 1)
+   {
+      return false;
+   }
+
+   // Otherwise the remainder is 1 so the number is most likely prime
+   return true;
+
 }
