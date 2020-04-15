196b3b1284daead6a373eaea1fe9080038a5369c
nshetler <nds39@zips.uakron.edu>
2020-04-15 14:54:58 -0400

    move code to function for getting data from file

--- Project3/nshetler_project3.cpp
+++ Project3/nshetler_project3.cpp
@@ -11,12 +11,14 @@
 #include <vector>
 #include <cmath>
 
+// This will be used to get the information from the file
+std::vector<std::vector<int>> getFileData(std::ifstream &imageFile, int &fileHorizontal, int &fileVertical, int &maxGrayscale);
 
 int main(int argc, char *argv[])
 {   
     if (argc < 4 || argc > 4)
     {
-        std::cout << "Wrong format! It should be \"a.exe image.pgm horizontal vertical\"";
+        std::cout << "Wrong format! It should be \"a.exe image.pgm horizontal vertical\"" << std::endl;
     }
    else
    {
@@ -31,58 +33,18 @@ int main(int argc, char *argv[])
         // See if file is open
         if (imageFile.is_open())
         {
-            // String for holding line of input
-            std::string fileLine = "";
+            // Variables for horizontal and vertical values in pgm file
+            int fileHorizontal = 0;
+            int fileVertical = 0;
 
-            // Get the first line
-            std::getline(imageFile, fileLine);
+            // Variable for max grayscale value
+            int maxGrayscale = 0;
 
-            // Check to make sure it is 'P2'
-            if (fileLine == "P2")
-            {
-                
-                // Get second line and do nothing with it
-                // (Line is a comment)
-                std::getline(imageFile, fileLine);
+            // Vector for reading in the other values in the file
+            std::vector<std::vector<int>> fileNumbers;
 
-                // Variables for horizontal and vertical values in pgm file
-                int fileHorizontal = 0;
-                int fileVertical = 0;
-
-                // Get the horizontal and vertical values from file
-                imageFile >> fileHorizontal >> fileVertical;
-               
-                // Get max grayscale value
-                int maxGrayscale = 0;
-                imageFile >> maxGrayscale;
-
-                // Vector for reading in the other values in the file
-                std::vector<std::vector<int>> fileNumbers(fileHorizontal, std::vector<int>(fileVertical, 0));
-
-                // Get all of the numbers from the file
-                for (int i = 0; i < fileHorizontal; ++i)
-                {
-                    for (int j = 0; j < fileVertical; ++j)
-                    { 
-                        imageFile >> fileNumbers[i][j];
-                    }
-                } 
-
-
-                // Output items from file
-                std::cout << "Horizontal: " << fileHorizontal << std::endl;
-                std::cout << "Vertical: " << fileVertical << std::endl;
-                std::cout << "Max Grayscale value: " << maxGrayscale << std::endl;
-
-                for (int i = 0; i < fileHorizontal; ++i)
-                {
-                    for (int j = 0; j < fileVertical; ++j)
-                    {
-                        std::cout << fileNumbers[i][j] << " ";
-                    }
-                    std::cout << std::endl;
-                }  
-            }
+            // Get the information from the file
+            fileNumbers = getFileData(imageFile, fileHorizontal, fileVertical, maxGrayscale);
         }
         else
         {
@@ -93,4 +55,61 @@ int main(int argc, char *argv[])
 	return 0;
 }
 
- 
+// Pre: This function will accept in an ifstream, and variables for storing information recieved from the file
+// Post: This function will set the variable based on the information recieved from the file
+std::vector<std::vector<int>> getFileData(std::ifstream &imageFile, int &fileHorizontal, int &fileVertical, int &maxGrayscale)
+{
+    // String for holding line of input
+    std::string fileLine = "";
+
+    // Get the first line
+    std::getline(imageFile, fileLine);
+
+    // Check to make sure it is 'P2'
+    if (fileLine == "P2")
+    {
+                
+        // Get second line and do nothing with it
+        // (Line is a comment)
+        std::getline(imageFile, fileLine);
+
+        // Get the horizontal and vertical values from file
+        imageFile >> fileHorizontal >> fileVertical;
+        
+        // Vector for reading in the other values in the file
+        std::vector<std::vector<int>> fileNumbers(fileHorizontal, std::vector<int>(fileVertical, 0));
+
+        // Get max grayscale value
+        imageFile >> maxGrayscale;
+        
+        // Get all of the numbers from the file and store in fileNumbers matrix
+        for (int i = 0; i < fileHorizontal; ++i)
+        {
+            for (int j = 0; j < fileVertical; ++j)
+            { 
+                imageFile >> fileNumbers[i][j];
+            }
+        } 
+
+        // Output items from file
+        std::cout << "Horizontal: " << fileHorizontal << std::endl;
+        std::cout << "Vertical: " << fileVertical << std::endl;
+        std::cout << "Max Grayscale value: " << maxGrayscale << std::endl;
+
+        std::cout << "Contents of the file: " << std::endl;
+        for (int i = 0; i < fileHorizontal; ++i)
+        {
+            for (int j = 0; j < fileVertical; ++j)
+            {
+                std::cout << fileNumbers[i][j] << " ";
+            }
+            std::cout << std::endl;
+        }  
+
+        return fileNumbers;
+    }
+    else
+    {
+        std::cout << "The file is not in the correct format" << std::endl;
+    }
+}
